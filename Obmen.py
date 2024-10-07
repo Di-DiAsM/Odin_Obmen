2c67d16858ea8c2fc69bf94682cd7a7c6ef28027
Di <dimas2343@mail.ru>
2024-10-07 16:37:54 +0300

    Изменен формат вывода курса. Теперь выводится только 2 знака после запятой

--- Obmen.py
+++ Obmen.py
@@ -1,3 +1,6 @@
+from http.client import responses
+from tkinter.messagebox import showerror
+
 import requests
 import json
 import pprint
@@ -5,7 +8,22 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 def exchange():
+    code = entry.get()
 
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()
+            data = response.json()
+            if code in data["rates"]:
+                exchange_rate = data["rates"][code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за 1 доллар")
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена")
+        except Exception as e:
+            mb/showerror("Ошибка", f"Произошла ошибка: {e}.")
+    else:
+        mb.showwarning("Внимание!", "Введите курс валюты!")
 
 window = Tk()
 window.title("Курсы обмена валют")
@@ -17,6 +35,8 @@ entry = Entry()
 entry.pack(padx=10, pady=10)
 
 Button(text="Получить курс обмена к доллару", command=exchange).pack(padx=10, pady=10)
+
+window.mainloop()
 
 
 
